e21ce84a9bd3debda71d5b68ad9cd06a428bc570
Ritvars Rundzāns <ritvars.rundzans@makit.lv>
2019-04-23 12:13:21 +0300

    require ruby 2.4; automatically handle opcua connections when "start" helper is used

--- lib/opcua_client.rb
+++ lib/opcua_client.rb
@@ -1,4 +1,13 @@
 module OPCUAClient
+  class << self
+    def start(*args)
+      client = OPCUAClient::Client.new
+      client.connect(*args)
+      yield client
+    ensure
+      client.disconnect
+    end
+  end
 end
 
 require "opcua_client/opcua_client"
