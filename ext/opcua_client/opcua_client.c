414d1d7e2417c00dcef76535f5100f6d256bb1bd
Ritvars Rundzāns <ritvars.rundzans@makit.lv>
2019-04-23 14:46:33 +0300

    add RW support for int32, float, boolean

--- ext/opcua_client/opcua_client.c
+++ ext/opcua_client/opcua_client.c
@@ -57,7 +57,7 @@ static VALUE rb_disconnect(VALUE self) {
     return RB_UINT2NUM(status);
 }
 
-static VALUE rb_writeInt16Value(VALUE self, VALUE v_nsIndex, VALUE v_name, VALUE v_newValue) {
+static VALUE rb_writeUaValue(VALUE self, VALUE v_nsIndex, VALUE v_name, VALUE v_newValue, int uaType) {
     if (RB_TYPE_P(v_name, T_STRING) != 1) {
         return raise_invalid_arguments_error();
     }
@@ -66,13 +66,12 @@ static VALUE rb_writeInt16Value(VALUE self, VALUE v_nsIndex, VALUE v_name, VALUE
         return raise_invalid_arguments_error();
     }
     
-    if (RB_TYPE_P(v_newValue, T_FIXNUM) != 1) {
+    if (uaType == UA_TYPES_INT16 && RB_TYPE_P(v_newValue, T_FIXNUM) != 1) {
         return raise_invalid_arguments_error();
     }
     
     char *name = StringValueCStr(v_name);
     int nsIndex = FIX2INT(v_nsIndex);
-    UA_Int16 newValue = FIX2INT(v_newValue);
     
     UA_Client *client;
     TypedData_Get_Struct(self, UA_Client, &UA_Client_Type, client);
@@ -80,10 +79,30 @@ static VALUE rb_writeInt16Value(VALUE self, VALUE v_nsIndex, VALUE v_name, VALUE
     UA_Variant value;
     UA_Variant_init(&value);
     
-    value.data = UA_malloc(sizeof(UA_Int16));
-    *(UA_Int16*)value.data = newValue;
-    value.type = &UA_TYPES[UA_TYPES_INT16];
-    
+    if (uaType == UA_TYPES_INT16) {
+        UA_Int16 newValue = NUM2SHORT(v_newValue);
+        value.data = UA_malloc(sizeof(UA_Int16));
+        *(UA_Int16*)value.data = newValue;
+        value.type = &UA_TYPES[UA_TYPES_INT16];
+    } else if (uaType == UA_TYPES_INT32) {
+        UA_Int32 newValue = NUM2INT(v_newValue);
+        value.data = UA_malloc(sizeof(UA_Int32));
+        *(UA_Int32*)value.data = newValue;
+        value.type = &UA_TYPES[UA_TYPES_INT32];
+    } else if (uaType == UA_TYPES_FLOAT) {
+        UA_Float newValue = NUM2DBL(v_newValue);
+        value.data = UA_malloc(sizeof(UA_Float));
+        *(UA_Float*)value.data = newValue;
+        value.type = &UA_TYPES[UA_TYPES_FLOAT];
+    } else if (uaType == UA_TYPES_BOOLEAN) {
+        UA_Boolean newValue = RTEST(v_newValue);
+        value.data = UA_malloc(sizeof(UA_Boolean));
+        *(UA_Boolean*)value.data = newValue;
+        value.type = &UA_TYPES[UA_TYPES_BOOLEAN];
+    } else {
+        rb_raise(cError, "Unsupported type");
+    }
+
     UA_StatusCode status = UA_Client_writeValueAttribute(client, UA_NODEID_STRING(nsIndex, name), &value);
     
     if (status == UA_STATUSCODE_GOOD) {
@@ -100,7 +119,23 @@ static VALUE rb_writeInt16Value(VALUE self, VALUE v_nsIndex, VALUE v_name, VALUE
     return Qnil;
 }
 
-static VALUE rb_readInt16Value(VALUE self, VALUE v_nsIndex, VALUE v_name) {
+static VALUE rb_writeInt16Value(VALUE self, VALUE v_nsIndex, VALUE v_name, VALUE v_newValue) {
+    return rb_writeUaValue(self, v_nsIndex, v_name, v_newValue, UA_TYPES_INT16);
+}
+
+static VALUE rb_writeInt32Value(VALUE self, VALUE v_nsIndex, VALUE v_name, VALUE v_newValue) {
+    return rb_writeUaValue(self, v_nsIndex, v_name, v_newValue, UA_TYPES_INT32);
+}
+
+static VALUE rb_writeBooleanValue(VALUE self, VALUE v_nsIndex, VALUE v_name, VALUE v_newValue) {
+    return rb_writeUaValue(self, v_nsIndex, v_name, v_newValue, UA_TYPES_BOOLEAN);
+}
+
+static VALUE rb_writeFloatValue(VALUE self, VALUE v_nsIndex, VALUE v_name, VALUE v_newValue) {
+    return rb_writeUaValue(self, v_nsIndex, v_name, v_newValue, UA_TYPES_FLOAT);
+}
+
+static VALUE rb_readUaValue(VALUE self, VALUE v_nsIndex, VALUE v_name, int type) {
     if (RB_TYPE_P(v_name, T_STRING) != 1) {
         return raise_invalid_arguments_error();
     }
@@ -129,10 +164,19 @@ static VALUE rb_readInt16Value(VALUE self, VALUE v_nsIndex, VALUE v_name) {
     
     VALUE result = Qnil;
     
-    if (UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_INT16])) {
+    if (type == UA_TYPES_INT16 && UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_INT16])) {
         UA_Int16 val =*(UA_Int16*)value.data;
         // printf("the value is: %i\n", val);
         result = INT2FIX(val);
+    } else if (type == UA_TYPES_INT32 && UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_INT32])) {
+        UA_Int32 val =*(UA_Int32*)value.data;
+        result = INT2FIX(val);
+    } else if (type == UA_TYPES_BOOLEAN && UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_BOOLEAN])) {
+        UA_Boolean val =*(UA_Boolean*)value.data;
+        result = val ? Qtrue : Qfalse;
+    } else if (type == UA_TYPES_FLOAT && UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_FLOAT])) {
+        UA_Float val =*(UA_Float*)value.data;
+        result = DBL2NUM(val);
     } else {
         rb_raise(cError, "Not an int16");
         return Qnil;
@@ -142,6 +186,22 @@ static VALUE rb_readInt16Value(VALUE self, VALUE v_nsIndex, VALUE v_name) {
     UA_Variant_deleteMembers(&value);
     
     return result;
+}
+
+static VALUE rb_readInt16Value(VALUE self, VALUE v_nsIndex, VALUE v_name) {
+    return rb_readUaValue(self, v_nsIndex, v_name, UA_TYPES_INT16);
+}
+
+static VALUE rb_readInt32Value(VALUE self, VALUE v_nsIndex, VALUE v_name) {
+    return rb_readUaValue(self, v_nsIndex, v_name, UA_TYPES_INT32);
+}
+
+static VALUE rb_readBooleanValue(VALUE self, VALUE v_nsIndex, VALUE v_name) {
+    return rb_readUaValue(self, v_nsIndex, v_name, UA_TYPES_BOOLEAN);
+}
+
+static VALUE rb_readFloatValue(VALUE self, VALUE v_nsIndex, VALUE v_name) {
+    return rb_readUaValue(self, v_nsIndex, v_name, UA_TYPES_FLOAT);
 }
 
 VALUE rb_get_human_UA_StatusCode(VALUE self, VALUE v_code) {
@@ -163,8 +223,18 @@ void Init_opcua_client()
     rb_define_alloc_func(cClient, allocate);
     rb_define_method(cClient, "connect", rb_connect, 1);
     rb_define_method(cClient, "disconnect", rb_disconnect, 0);
+    
     rb_define_method(cClient, "read_int16", rb_readInt16Value, 2);
+    rb_define_method(cClient, "read_int32", rb_readInt32Value, 2);
+    rb_define_method(cClient, "read_float", rb_readFloatValue, 2);
+    rb_define_method(cClient, "read_boolean", rb_readBooleanValue, 2);
+    rb_define_method(cClient, "read_bool", rb_readBooleanValue, 2);
+    
     rb_define_method(cClient, "write_int16", rb_writeInt16Value, 3);
+    rb_define_method(cClient, "write_int32", rb_writeInt32Value, 3);
+    rb_define_method(cClient, "write_float", rb_writeFloatValue, 3);
+    rb_define_method(cClient, "write_boolean", rb_writeBooleanValue, 3);
+    rb_define_method(cClient, "write_bool", rb_writeBooleanValue, 3);
     
     rb_define_singleton_method(mOPCUAClient, "human_status_code", rb_get_human_UA_StatusCode, 1);
 }
