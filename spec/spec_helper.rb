94eea5b02aeaf51d412a711c56a6edb6157308bc
ritvars <rrundzans@gmail.com>
2022-08-25 20:48:40 +0300

    verify_compaction_references

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,22 @@
 require 'rspec'
 require 'opcua_client'
 
-RSpec.configure do |config|
-  def new_client(connect: true)
-    client = OPCUAClient::Client.new
+# https://github.com/brianmario/mysql2/commit/0ee20536501848a354f1c3a007333167120c7457
+if GC.respond_to?(:verify_compaction_references)
+  # This method was added in Ruby 3.0.0. Calling it this way asks the GC to
+  # move objects around, helping to find object movement bugs.
+  GC.verify_compaction_references(double_heap: true, toward: :empty)
+end
 
-    if connect
-      # TODO
-    end
+def new_client(connect: true)
+  client = OPCUAClient::Client.new
 
-    client
+  if connect
+    # TODO
   end
+
+  client
+end
+
+RSpec.configure do |config|
 end
